e2bc6e2c44095e40d40432939409bf4d7fcfa363
delbert harry <delbertharry@gmail.com>
2020-04-29 04:08:52 -0400

    Basic DLL

---
+++ TempService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace WcfLibrary
+{
+    [ServiceContract]
+    public interface ITempService
+    {
+        [OperationContract]
+        String GetTest();
+        [OperationContract]
+        String GetDelbertTest(String pName);
+    }
+
+    public class TempService : ITempService
+    {
+        public String GetTest()
+        {
+            return "Bonjour " + this.ToString();
+        }
+
+        public String GetDelbertTest(String pName)
+        {
+            return "Non mwen sé Delbert " + pName + " : " + DateTime.Now.ToString();
+        }
+    }
+}
